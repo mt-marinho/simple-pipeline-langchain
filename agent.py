c7cf7d5b046e0e7be499bad45d3ce593fc62db51
MATHEUS MARINHO DE ESPINDULA <e.matheus.marinho@tjms.jus.br>
2026-07-29 18:55:40 -0400

    memory

--- agent.py
+++ agent.py
@@ -1,5 +1,4 @@
 
-import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain.agents import create_agent
@@ -11,10 +10,23 @@ model = init_chat_model("google_genai:gemini-3.5-flash")
 
 my_agent = create_agent(
     model=model, 
-    system_prompt="se voce não souber a resposta busque no google usando o tavily search",
-    tools=[TavilySearch()]
+    system_prompt="responda oque voce sabe, se não souber diga que não sabe, não tente inventar respostas",
+    tools=[TavilySearch()],
 )
 
-pergunta = "busque a temperatura de campo grande ms atualmente usando o tevily"
-resposta = my_agent.invoke({"messages": [{"role": "user", "content": pergunta}]})
-print(resposta["messages"][-1].text)
+config = {"configurable": {"thread_id": "nome"}}
+
+
+def run_cli():
+    print("agente em funcionamento")
+    while True:
+        user_input = input("Digite sua pergunta: ")
+        response = my_agent.invoke(
+            {"messages": [{"role": "user", "content": user_input}]},
+            config=config,
+        )
+        print(f"Agente: {response['messages'][-1].content}")
+
+
+if __name__ == "__main__":
+    run_cli()
